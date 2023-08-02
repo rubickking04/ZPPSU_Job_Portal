20bfde23506703f590caf9d247d77373eb4582b6
Al-Fhaigar J. Usman <rubickking04@gmail.com>
2023-08-02 11:50:02 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\LoginController as AuthAdminLogin;
+use App\Http\Controllers\Admin\Auth\LogoutController as AuthAdminLogout;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 
 /*
@@ -19,13 +20,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['middleware' => 'prevent-back-history'], function () {
+    /*
+    |--------------------------------------------------------------------------
+    | User Web Routes
+    |--------------------------------------------------------------------------
+    */
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Web Routes
+    |--------------------------------------------------------------------------
+    */
     Route::controller(AuthAdminLogin::class)->group( function() {
         Route::get('/admin/login', 'index')->name('admin.login')->middleware('guest:admin');
         Route::post('/admin/login', 'login')->name('admin.auth.login');
     });
     Route::middleware('auth:admin')->group(function () {
         Route::controller(AdminHomeController::class)->group(function() {
-            Route::get('admin/home', 'index')->name('admin.home');
+            Route::get('/admin/home', 'index')->name('admin.home');
+        });
+        Route::controller(AuthAdminLogout::class)->group(function() {
+            Route::post('/admin/logout', 'logout')->name('admin.logout');
         });
     });
 });
